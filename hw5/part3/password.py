9488bad8665aa771e9b64a557931d2442d42b2d8
Cate-Lukner <46268240+Cate-Lukner@users.noreply.github.com>
2020-03-01 15:27:11 -0600

    part 3 completed, all parts now completed

---
+++ hw5/part3/password.py
@@ -0,0 +1,22 @@
+''' Strong Password Detection
+
+Author: <your name>
+'''
+
+import re
+
+def strong_password(string):
+    '''Given a string, return True if is strong, False otherwise
+
+    A strong password is definied as one that is at least eight
+    characters logn, contains both uppercase and lowercase characters,
+    and has at least one digit.
+
+    '''
+    # Regex to test for at least 8 characters, one uppercase, one lowercase, and one digit
+    if re.match(r'[A-Za-z0-9]{8,}', string):
+        return True
+    else:
+        return False
+    
+

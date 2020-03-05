61d499a34dc4cbf1c7b0bafef876fc8002841265
Cate-Lukner <46268240+Cate-Lukner@users.noreply.github.com>
2020-03-05 07:52:06 -0600

    removed midterm

--- midterm/create_task_2_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create files most_common_source.txt and most_common_dest.txt in each directory
-for i in {0..9}
-do
-		cd ~/Documents/Github/csc221/midterm
-		touch dir${i}/most_common_source.txt
-		touch dir${i}/most_common_dest.txt
-done
--- midterm/move_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {0..10}
-do 
-		mv *${i}.txt dir${i}
-done

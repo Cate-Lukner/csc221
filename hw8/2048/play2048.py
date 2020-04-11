502263fc7d346b43227ed65d4cd0cb08bdd47a87
Cate-Lukner <46268240+Cate-Lukner@users.noreply.github.com>
2020-04-10 23:44:56 -0500

    Finished Part 5

--- hw8/2048/play2048.py
+++ hw8/2048/play2048.py
@@ -7,34 +7,53 @@ Author: Cate Lukner
 '''
 
 # imports
-import argparse
 import pyautogui as pag
 import time
 import webbrowser
 
+URL = 'https://gabrielecirulli.github.io/2048/'
+RUN_TIME = 30 
+PAUSE_TIME = 5
+
 def open_webbrowser(url):
+    ''' Opens the game '''
     # Open the game 
     webbrowser.open(url)
     print('Be sure not to click out of the browser that will open!')
-    # To give time for my slow internet connection to load
+    # To accomodate my slow internet
     time.sleep(5)
-    return active_window
 
-def play_game(game_window):
-    # TODO: boolean - test if game screen is active
-    # TODO: Check for gameover conditons - end game once over
-    # TODO: Add pause period of 5 seconds every 30 seconds
-    while True:
-        pag.press(['up', 'right', 'down', 'left'])
+def play_game():
+    ''' Follows the up, right, down, left arrow key pattern to play.'''
+    pag.press(['up', 'right', 'down', 'left'])
+
+def check_game_over():
+    ''' Checks for Try Again button on the screen. If it does not exist, it
+    returns True. If the Try Again button does exist, it returns False.'''
+    # Locate Try Again button
+    try_again = pag.locateOnScreen('try_again_button.png', confidence=0.8)   
+    # Return True or False based on existence of Try Again button
+    if not try_again:
+        return True
+    elif try_again:
+        return False
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='The URL to the form to be filled.')
+    '''Main function'''
+    # Open game
+    open_webbrowser(URL)
 
-    args = parser.parse_args()
-
-    game_window = open_webbrowser(args.url)
-    play_game(game_window)
+    # Set the start time
+    time_start = time.time()
+    
+    # Play game for the set run time then pause
+    while check_game_over():
+        if time.time() < time_start + RUN_TIME:
+            play_game()
+        else:
+            print('Here\'s your chance to kill the script!')
+            time.sleep(PAUSE_TIME)
+            time_start = time.time()
 
 if __name__=='__main__':
     main()
